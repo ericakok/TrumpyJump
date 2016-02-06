d6b2af28517e107e5c7cff3b4b7ff8d7b3264895
Jesus Argel <jesusargel@wg-dhcp33d018.fsuwin.wireless.fsu.edu>
2016-02-06 15:10:08 -0500

    Created background

--- TrumpyJump/GameScene.swift
+++ TrumpyJump/GameScene.swift
@@ -10,33 +10,24 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        //Physics
+        self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -0.5)
         
-        self.addChild(myLabel)
+        //Wig
+        
+        
+        //Background
+        backgroundColor = UIColor(colorLiteralRed: 159.0/255.0, green: 201.0/255.0, blue: 241.0/255.0, alpha: 1.0)
+        let ground = SKSpriteNode(color:UIColor.brownColor(), size: CGSize(width: view.frame.size.width, height: 20))
+        ground.position = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height * 0.0)
+        addChild(ground)
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        
     }
    
     override func update(currentTime: CFTimeInterval) {
