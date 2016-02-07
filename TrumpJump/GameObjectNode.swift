dfa2f55b4fd615803eea62ecec8c5047b9a39bf0
Jesus Argel <jesusargel@wg-dhcp33d018.fsuwin.wireless.fsu.edu>
2016-02-07 00:14:07 -0500

    Added physics and star object

---
+++ TrumpJump/GameObjectNode.swift
@@ -0,0 +1,13 @@
+//
+//  GameObjectNode.swift
+//  TrumpJump
+//
+//  Created by Jesus Argel on 2/7/16.
+//  Copyright © 2016 Erica Marie Kok. All rights reserved.
+//
+
+import UIKit
+
+class GameObjectNode: SKNode {
+
+}
