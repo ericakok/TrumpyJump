5a0617bb1a540e243ac5ec0b172b483a0005ce12
Jesus Argel <jesusargel@wg-dhcp33d018.fsuwin.wireless.fsu.edu>
2016-02-07 00:19:56 -0500

    Physics and Star Object

--- TrumpJump/GameObjectNode.swift
+++ TrumpJump/GameObjectNode.swift
@@ -6,8 +6,34 @@
 //  Copyright © 2016 Erica Marie Kok. All rights reserved.
 //
 
-import UIKit
+import SpriteKit
+struct CollisionCategoryBitmask {
+    static let Player: UInt32 = 0x00
+    static let Star: UInt32 = 0x01
+    static let Platform: UInt32 = 0x02
+}
 
 class GameObjectNode: SKNode {
+    func collisionWithPlayer(player: SKNode) -> Bool {
+        return false
+    }
+    
+    func checkNodeRemoval(playerY: CGFloat) {
+        if playerY > self.position.y + 300.0 {
+            self.removeFromParent()
+        }
+    }
+}
 
+class StarNode: GameObjectNode {
+    override func collisionWithPlayer(player: SKNode) -> Bool {
+        // Boost the player up
+        player.physicsBody?.velocity = CGVector(dx: player.physicsBody!.velocity.dx, dy: 400.0)
+        
+        // Remove this Star
+        self.removeFromParent()
+        
+        // The HUD needs updating to show the new stars and score
+        return true
+    }
 }
--- TrumpJump/GameScene.swift
+++ TrumpJump/GameScene.swift
@@ -8,13 +8,18 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate{
     
+    
+    // Tap To Start node
+    let tapToStartNode = SKSpriteNode(imageNamed: "UberJumpGraphics/Assets.atlas/TapToStart")
     // Layered Nodes
     var backgroundNode: SKNode!
     var midgroundNode: SKNode!
     var foregroundNode: SKNode!
     var hudNode: SKNode!
+    // Player
+    var player: SKNode!
     
     
     // To Accommodate iPhone 6
@@ -28,18 +33,37 @@ class GameScene: SKScene {
     override init(size: CGSize) {
         super.init(size: size)
         backgroundColor = SKColor.whiteColor()
+        // Add some gravity
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
         scaleFactor = self.size.width / 320.0
-        
+        // Set contact delegate
+        physicsWorld.contactDelegate = self
         // Create the game nodes
         // Background
         backgroundNode = createBackgroundNode()
         addChild(backgroundNode)
+        // Foreground
+        foregroundNode = SKNode()
+        addChild(foregroundNode)
+        // HUD
+        hudNode = SKNode()
+        addChild(hudNode)
+        // Add the player
+        player = createPlayer()
+        foregroundNode.addChild(player)
+        // Tap to Start
+        tapToStartNode.position = CGPoint(x: self.size.width / 2, y: 180.0)
+        hudNode.addChild(tapToStartNode)
+        // Add a star
+        let star = createStarAtPosition(CGPoint(x: 160, y: 220))
+        foregroundNode.addChild(star)
 
         }
     
 
     
     func createBackgroundNode() -> SKNode {
+        
         // 1
         // Create the node
         let backgroundNode = SKNode()
@@ -65,4 +89,96 @@ class GameScene: SKScene {
         // Return the completed background node
         return backgroundNode
     }
+    
+    func createPlayer() -> SKNode {
+        let playerNode = SKNode()
+        playerNode.position = CGPoint(x: self.size.width / 2, y: 80.0)
+        
+        let sprite = SKSpriteNode(imageNamed: "UberJumpGraphics/Assets.atlas/Player")
+        playerNode.addChild(sprite)
+        
+        // 1
+        playerNode.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
+        // 2
+        playerNode.physicsBody?.dynamic = false
+        // 3
+        playerNode.physicsBody?.allowsRotation = false
+        // 4
+        playerNode.physicsBody?.restitution = 1.0
+        playerNode.physicsBody?.friction = 0.0
+        playerNode.physicsBody?.angularDamping = 0.0
+        playerNode.physicsBody?.linearDamping = 0.0
+        
+        // 1
+        playerNode.physicsBody?.usesPreciseCollisionDetection = true
+        // 2
+        playerNode.physicsBody?.categoryBitMask = CollisionCategoryBitmask.Player
+        // 3
+        playerNode.physicsBody?.collisionBitMask = 0
+        // 4
+        playerNode.physicsBody?.contactTestBitMask = CollisionCategoryBitmask.Star | CollisionCategoryBitmask.Platform
+        
+        return playerNode
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        // 1
+        // If we're already playing, ignore touches
+        if player.physicsBody!.dynamic {
+            return
+        }
+        
+        // 2
+        // Remove the Tap to Start node
+        tapToStartNode.removeFromParent()
+        
+        // 3
+        // Start the player by putting them into the physics simulation
+        player.physicsBody?.dynamic = true
+        
+        // 4
+        player.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 20.0))
+    }
+    
+    func createStarAtPosition(position: CGPoint) -> StarNode {
+        // 1
+        let node = StarNode()
+        let thePosition = CGPoint(x: position.x * scaleFactor, y: position.y)
+        node.position = thePosition
+        node.name = "NODE_STAR"
+        
+        // 2
+        var sprite: SKSpriteNode
+        sprite = SKSpriteNode(imageNamed: "UberJumpGraphics/Assets.atlas/Star")
+        node.addChild(sprite)
+        
+        // 3
+        node.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
+        
+        // 4
+        node.physicsBody?.dynamic = false
+        
+        node.physicsBody?.categoryBitMask = CollisionCategoryBitmask.Star
+        node.physicsBody?.collisionBitMask = 0
+        
+        return node
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        // 1
+        var updateHUD = false
+        
+        // 2
+        let whichNode = (contact.bodyA.node != player) ? contact.bodyA.node : contact.bodyB.node
+        let other = whichNode as! GameObjectNode
+        
+        // 3
+        updateHUD = other.collisionWithPlayer(player)
+        
+        // Update the HUD if necessary
+        if updateHUD {
+            // 4 TODO: Update HUD in Part 2
+        }
+    }
+
 }
